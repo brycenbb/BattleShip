27c0916ceff7775919d4307c5f5ae0c4b95f6e51
brycen <brycenbb1996@gmail.com>
2022-04-22 12:03:01 +0100

    board is not populated correctly, places into each row irregardless of length

--- factories.js
+++ factories.js
@@ -17,6 +17,58 @@ const shipFactory = (number) => {
   return { hit };
 };
 
-const gameBoard = () => {};
+const gameBoard = () => {
+  //board is a 10x10 space. Divide by 10 and floor it to get the row, mod by 10 to get the column
+  let board = new Array(10).fill(new Array(10).fill(''));
+  let ships = [];
+  let counter = 0;
 
-module.exports = shipFactory;
+  const placeShip = (x, y, length, direction) => {
+    // console.log(counter);
+    // counter++;
+    console.log(board);
+    if (!validPlacement()) {
+      //Need to throw an error here in some way, but will return false for now.
+      return false;
+    }
+    ships.push(shipFactory(length));
+    console.log('length:', length);
+    console.log('size:', length, '+', x);
+    if (direction === 'X') {
+      for (let i = x; i < length + x; i++) {
+        // board[i][y] = ships[ships.length - 1];
+        board[i][y] = 'X';
+      }
+    } else {
+      for (let i = y; i < length + y; i++) {
+        // board[x][i] = ships[ships.length - 1];
+        board[x][i] = 'X';
+      }
+    }
+    console.log(board);
+
+    return true;
+  };
+
+  const validPlacement = (x, y, length, direction) => {
+    if (direction === 'X') {
+      for (let i = x; i < length + x; i++) {
+        if (board[i][y] != '') {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      for (let i = y; i < length + y; i++) {
+        if (board[x][i] != '') {
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+
+  return { placeShip };
+};
+
+module.exports = { shipFactory, gameBoard };
--- factories.test.js
+++ factories.test.js
@@ -1,10 +1,20 @@
 const { test, expect } = require('@jest/globals');
-const shipFactory = require('./factories');
+const factories = require('./factories');
 
 test('Ship takes hits and sinks', () => {
-  expect(shipFactory(1).hit(0)).toBe(true);
+  expect(factories.shipFactory(1).hit(0)).toBe(true);
 });
 
 test('Ship takes hits and does not sink', () => {
-  expect(shipFactory(2).hit(1)).toBe(false);
+  expect(factories.shipFactory(2).hit(1)).toBe(false);
+});
+
+test('ship placement works 1', () => {
+  expect(factories.gameBoard().placeShip(2, 2, 3, 'X')).toBe(true);
+});
+
+test('ship placement works 2', () => {
+  let board = factories.gameBoard();
+  board.placeShip(2, 2, 3, 'X');
+  expect(board.placeShip(2, 2, 3, 'X')).toBe(true);
 });
