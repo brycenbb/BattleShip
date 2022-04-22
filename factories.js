f583ccafaf4a4e43c6ec95f41aef74627fc91407
brycen <brycenbb1996@gmail.com>
2022-04-22 11:05:52 +0100

    ship factory made and tested

---
+++ factories.js
@@ -0,0 +1,22 @@
+const shipFactory = (number) => {
+  let shipLength = number;
+  let hitStatus = Array(shipLength).fill('');
+
+  const hit = (position) => {
+    hitStatus[position] = 'X';
+    return isSunk();
+  };
+
+  const isSunk = () => {
+    if (hitStatus.filter((index) => index === 'X').length === shipLength) {
+      return true;
+    }
+    // console.log('ship status:', hitStatus);
+    return false;
+  };
+  return { hit };
+};
+
+const gameBoard = () => {};
+
+module.exports = shipFactory;
